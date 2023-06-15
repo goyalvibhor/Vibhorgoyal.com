c5ad33da05d0f5a072d2749d733d72d2b2aa9c14
goyalvibhor <vibhordelhu@gmail.com>
2023-06-15 10:54:37 +0530

    ll

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ let header = $(`
         <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
         <li class="nav-item nav-item-hover"><a class="nav-link" href="portfolio.html">Portfolio</a></li>
         <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-        <li class="nav-item nav-item-hover"><a class="nav-link" href="blog.html" >Blogs</a></li>
+        <li class="nav-item nav-item-hover"><a class="nav-link" href="blog.html">Blogs</a></li>
         <li class="nav-item">
           <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()">
         </li>
